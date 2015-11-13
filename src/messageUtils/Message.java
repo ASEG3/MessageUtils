dd35c74fa8f42eb3ed601ade3a62125bac4d0204
JordanHitchman <jordan.hitchman@hotmail.co.uk>
2015-11-13 12:23:37 +0000

    Updated

---
+++ src/messageUtils/Message.java
@@ -0,0 +1,66 @@
+package messageUtils;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Message implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private ArrayList<ArrayList<Double>> weightedAverage;
+	private double mostExpensive;
+	private double leastExpensive;
+	private ArrayList<ArrayList<String>> newHouses;
+
+	public Message() {
+		weightedAverage = new ArrayList<ArrayList<Double>>();
+		newHouses = new ArrayList<ArrayList<String>>();
+	}
+
+	public void addHouseEntryNew(ArrayList<String> houseEntry) {
+		newHouses.add(houseEntry);
+	}
+
+	public ArrayList<String> getHouseInfo(int i) {
+		return newHouses.get(i);
+	}
+
+	public int getSizeOfNewHouses() {
+		return newHouses.size();
+	}
+
+	public ArrayList<ArrayList<String>> getHouses() {
+		return newHouses;
+	}
+
+	public ArrayList<Double> getHouse(int i) {
+		return weightedAverage.get(i);
+	}
+
+	public void addWeight(ArrayList<Double> houseWeights) {
+		weightedAverage.add(houseWeights);
+	}
+
+	public int getSizeOfWeighted() {
+		return weightedAverage.size();
+	}
+
+	public ArrayList<ArrayList<Double>> getHouse() {
+		return weightedAverage;
+	}
+
+	public void setLeastExpensive(double leastExpensive) {
+		this.leastExpensive = leastExpensive;
+	}
+
+	public double getLeastExpensive() {
+		return leastExpensive;
+	}
+
+	public void setMostExpensive(double mostExpensive) {
+		this.mostExpensive = mostExpensive;
+	}
+
+	public double getMostExpensive() {
+		return mostExpensive;
+	}
+
+}
